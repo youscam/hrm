385082ccb66748ada6593ee4a456d982880743f3
kamal_azzouzi <youscam@outlook.com>
2017-04-17 23:20:55 +0100

    end

---
+++ src/main/java/ma/youscam/HrmApplication.java
@@ -0,0 +1,12 @@
+package ma.youscam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmApplication.class, args);
+	}
+}
